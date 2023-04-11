335003248e77eb638d08d0601685d3638e4abe08
unofficialnikke <nicklasnystrm3@gmail.com>
2023-04-11 15:27:44 +0300

    added firebase firestore database

--- components/NewTraining.tsx
+++ components/NewTraining.tsx
@@ -1,52 +1,99 @@
 import * as React from 'react';
-import { View, Text, FlatList, ScrollView, Alert } from 'react-native';
-import { Input, Button, Tooltip } from '@rneui/themed';
-import { useState } from 'react';
+import { View, Text, Alert, FlatList, SectionList } from 'react-native';
+import { Input, Button } from '@rneui/themed';
+import { useState, useEffect } from 'react';
 import { styles } from '../styles/NewTrainingStyle';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, onSnapshot, updateDoc, arrayUnion, doc, query } from 'firebase/firestore';
 
-type Training = [string, string, string, string]
-
-export type TrainingList = {
-    name: string;
-    training: Training[]
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyANpiYh4Wlo0FwfuygNMqxRbC2KgjnOBEs",
+    authDomain: "fitnessapp-42977.firebaseapp.com",
+    databaseURL: "fitnessapp-42977-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "fitnessapp-42977",
+    storageBucket: "fitnessapp-42977.appspot.com",
+    messagingSenderId: "1063700009118",
+    appId: "1:1063700009118:web:cbf8f859e07df1b0b62b8f"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default function NewTraining({ navigation }) {
-    const [trainingList, setTrainingList] = useState<Training[]>([])
-    const [trainingLists, setTrainingLists] = useState<TrainingList[]>([])
     const [trainingListName, setTrainingListName] = useState("")
     const [trainingName, setTrainingName] = useState("")
     const [weight, setWeight] = useState("")
     const [repetitions, setRepetitions] = useState("")
     const [amount, setAmount] = useState("")
+    const [trainingList, setTrainingList] = useState<any>([])
+    const [trainingListId, setTrainingListId] = useState<number>(1)
+    const [visible, setVisible] = useState(false)
 
-    const addTraining = () => {
-        const newTraining: Training = [trainingName, weight, repetitions, amount];
-        if (trainingName === "") {
-            Alert.alert("Virhe", "Liikkeen nimi ei voi olla tyhjä")
-            return
+    const addTraining = async () => {
+        try {
+            const newTraining = {
+                trainingName: trainingName,
+                weight: weight,
+                repetitions: repetitions,
+                amount: amount
+            };
+            await updateDoc(doc(db, "trainingList", trainingListId.toString()), {
+                trainings: arrayUnion(newTraining)
+            });
+            console.log('Training added: ', newTraining);
+        } catch (error) {
+            console.error('Error adding training: ', error);
         }
-        setTrainingList([...trainingList, newTraining]);
-        setTrainingName("");
-        setWeight("");
-        setRepetitions("");
-        setAmount("");
-
+        setTrainingName("")
+        setWeight("")
+        setRepetitions("")
+        setAmount("")
     };
 
-    const addTrainingList = () => {
-        const newTrainingList: TrainingList = {
-            name: trainingListName,
-            training: trainingList
-        };
-        if (trainingListName === "") {
-            Alert.alert("Virhe", "Harjoituksen nimi ei voi olla tyhjä")
-            return
+    const addTrainingList = async () => {
+        try {
+            const ref = await addDoc(collection(db, "trainingList"), {
+                name: trainingListName,
+                trainings: [],
+            });
+            {/* @ts-ignore */ }
+            setTrainingListId(ref.id);
+            console.log("Training list added with ID: ", ref.id);
+        } catch (error) {
+            console.error("Error adding training list: ", error);
         }
-        setTrainingLists([...trainingLists, newTrainingList])
         setTrainingListName("")
-        setTrainingList([])
-    }
+    };
+
+    useEffect(() => {
+        const q = query(collection(db, 'trainingList'));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const trainingListData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+            setTrainingList(trainingListData);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    const renderItem = ({ item }) => (
+        <View>
+            <Text style={{ fontSize: 22, textAlign: "center", marginBottom: 5, marginTop: 5 }}>{item.name}</Text>
+            <FlatList
+                data={item.trainings}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text style={{ fontSize: 20, textAlign: "center" }}>
+                            Liike: {item.trainingName}</Text>
+                        <Text style={{ fontSize: 18, textAlign: "center" }}>
+                            Painot: {item.weight},
+                            Toistot: {item.repetitions},
+                            Määrä: {item.amount}
+                        </Text>
+                    </View>
+                )}
+            />
+        </View>
+    )
 
     return (
         <View style={styles.container}>
@@ -55,13 +102,13 @@ export default function NewTraining({ navigation }) {
                 type="solid"
                 color="#414141"
                 title="Katso lisätyt harjoituksesi"
-                onPress={() =>
-                    navigation.navigate('ShowTrainings', { trainingLists: trainingLists })}
+                onPress={() => {
+                    navigation.navigate('ShowTrainings', trainingList)
+                }}
             />
             <Text style={{ fontSize: 20, marginBottom: 12, marginTop: 30 }}>Anna harjoitukselle nimi:</Text>
             <Input
                 style={styles.input}
-
                 placeholder="Harjoituksen nimi"
                 value={trainingListName}
                 onChangeText={t => {
@@ -102,23 +149,27 @@ export default function NewTraining({ navigation }) {
                 }}
             />
             <Button
+                title="Lisää liike"
                 style={{ padding: 10 }}
                 type="solid"
                 color="orange"
-                onPress={addTraining}
-                title="Lisää liike"
+                onPress={() => {
+                    addTraining()
+                }}
             />
-
-            <View style={{ marginTop: 30 }}>
-                <Button
-                    size="lg"
-                    type="solid"
-                    color="orange"
-                    onPress={addTrainingList}
-                    title="Lisää valmis harjoitus"
-                />
-            </View>
-
-        </View>
+            <Button
+                title="Lisää harjoitus"
+                style={{ padding: 10 }}
+                type="solid"
+                color="orange"
+                onPress={() => {
+                    addTrainingList()
+                }}
+            />
+            <FlatList
+                data={trainingList}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id} />
+        </View >
     )
 }
--- components/ShowTrainings.tsx
+++ components/ShowTrainings.tsx
@@ -1,53 +1,37 @@
 import * as React from 'react';
 import { View, FlatList, Text } from "react-native";
-import { TrainingList } from "./NewTraining";
 import { styles } from '../styles/ShowTrainingsStyle';
+import NewTraining from './NewTraining';
 
-interface Props {
-    route: {
-        params: {
-            trainingLists: TrainingList[];
-        };
-    };
-}
+export default function ShowTrainings({ route, navigation }) {
+    const trainings = route.params
 
-export default function ShowTrainings({ route }: Props) {
-    const { trainingLists } = route.params
-
-    const listSeparator = () => {
-        return (
-            <View style={{ height: 1, backgroundColor: "grey" }} />
-        );
-    }
-
+    const renderItem = ({ item }) => (
+        <View>
+            <Text style={{ fontSize: 22, textAlign: "center", marginBottom: 5, marginTop: 5 }}>{item.name}</Text>
+            <FlatList
+                data={item.trainings}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text style={{ fontSize: 20, textAlign: "center" }}>
+                            Liike: {item.trainingName}</Text>
+                        <Text style={{ fontSize: 18, textAlign: "center" }}>
+                            Painot: {item.weight},
+                            Toistot: {item.repetitions},
+                            Määrä: {item.amount}
+                        </Text>
+                    </View>
+                )}
+            />
+        </View>
+    )
     return (
         <View style={styles.container}>
-            {trainingLists.length === 0 ?
-                <Text style={{ fontSize: 22 }}>Ei vielä lisättyjä harjoituksia</Text>
-                :
-                <FlatList
-                    data={trainingLists}
-                    ItemSeparatorComponent={listSeparator}
-                    keyExtractor={(item) => item.name}
-                    renderItem={({ item }) => (
-                        <View style={{ marginBottom: 10 }}>
-                            <Text style={{ fontSize: 24, textAlign: "center" }}>{item.name}</Text>
-                            {item.training.map((training, index) => (
-                                <View key={index}>
-                                    <Text style={styles.map}>
-                                        {`${training[0]}`}
-                                    </Text>
-                                    <Text style={{ fontSize: 18 }}>
-                                        {`Painot: ${training[1]} kg, `}
-                                        {`Sarja: ${training[2]}, `}
-                                        {`Toistot: ${training[3]}x`}
-                                    </Text>
-                                </View>
-                            ))}
-                        </View>
-                    )}
-                />
-            }
+            <FlatList
+                data={trainings}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+            />
         </View>
     );
 };
