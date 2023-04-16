cfd2df98565d099af9afe0e50fea0fe31f88cec5
unofficialnikke <nicklasnystrm3@gmail.com>
2023-04-16 03:52:14 +0300

    updating

---
+++ config/apiConfig.tsx
@@ -0,0 +1,7 @@
+const config = {
+    API_KEY: "d7333264c3msh1dbb40b432c704ep132e6fjsn9e3526299d07",
+    API: "exercisedb.p.rapidapi.com",
+    API_URL: "https://exercisedb.p.rapidapi.com/exercises/bodyPart/"
+}
+
+export default config
